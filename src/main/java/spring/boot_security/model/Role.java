9cfdece751f24457840dcd75fb1bfd6eb3f7f842
MalkovAlex <amalkov681@gmail.com>
2023-03-03 05:21:54 +0500

    02-03-23 at 05 am

--- src/main/java/spring/boot_security/model/Role.java
+++ src/main/java/spring/boot_security/model/Role.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "role")
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
